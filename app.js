f52443a084ad8e331d10778d553c1b9744121291
Dmitry Davydov <info@solutecs.biz>
2017-03-04 22:34:01 +0100

    04-03-2017 22:34:01

---
+++ app.js
@@ -0,0 +1,132 @@
+const form = document.getElementById('registrar');
+const mainDiv = document.querySelector('.main');
+const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckbox = document.createElement('input');
+
+filterLabel.textContent = "Hide not confirmed";
+filterCheckbox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckbox);
+mainDiv.insertBefore(div, ul);
+
+// the event handler for the checkbox which filters
+// (showing or hiding) the invitees who confirmed or not
+filterCheckbox.addEventListener('change', (e) => {
+  const isChecked = e.target.checked;
+  let arr_li = ul.children;
+  if (isChecked) {
+    for(let i=0; i<arr_li.length; i++) {
+      if (arr_li[i].className !== 'responded')
+        arr_li[i].style.display = 'none';
+    }
+  } else {
+    for(let i=0; i<arr_li.length; i++) {
+      if (arr_li[i].className !== 'responded')
+        arr_li[i].style.display = '';
+    }
+  }
+});
+// the event handler for submit event: validates and
+// calls to createLI function which creates and
+// appends a new li element with the info about the invitee
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = "";
+//validation of the submitted text from input
+  let validated = validateInput(text);
+  if (validated) {
+    const li = createLI(text);
+    ul.appendChild(li);
+  }
+});
+
+function validateInput(submittedText) {
+  if(submittedText !=="") {
+    alert("kuku!");
+    setToLocalStorage(submittedText);
+    return true;
+  }
+}
+
+function setToLocalStorage(submittedText) {
+  let id = 0;
+  localStorage.setItem(id, submittedText)
+}
+
+function createLI(text) {
+
+  function createEL(elemName, prop, val) {
+    const elem = document.createElement(elemName);
+    elem[prop] = val;
+    return elem;
+  }
+
+  function appendToLI(elemName, prop, val) {
+    const element = createEL(elemName, prop, val);
+    li.appendChild(element);
+    return element;
+  }
+
+  const li = document.createElement('li');
+  appendToLI('span', 'textContent', text);
+  appendToLI('label', 'textContent', 'Confirmed')
+  .appendChild(createEL('input', 'type', 'checkbox'));
+  appendToLI('button', 'textContent', 'edit');
+  appendToLI('button', 'textContent', 'delete');
+
+  return li;
+}
+// the event handler for checking and unchecking a particular
+// invitee weather it confirmed or not
+ul.addEventListener('change', (e) => {
+
+  const checkbox = e.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+
+  if(checked) {
+    listItem.className = 'responded';
+  } else {
+    listItem.className = '';
+  }
+});
+
+//event handler for cklicks on buttons 'delete','edit','save'
+ul.addEventListener('click', (e) => {
+
+  if (e.target.tagName === 'BUTTON') {
+    const button = e.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    const action = button.textContent;
+    const nameActions = {
+      delete: () => {
+        ul.removeChild(li);
+      },
+      edit: () => {
+        const span = li.firstElementChild;
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = span.innerHTML;
+        li.insertBefore(input, span);
+        li.removeChild(span);
+        button.textContent = 'save';
+      },
+      save: () => {
+        const input = li.firstElementChild;
+        const span = document.createElement('span');
+        span.textContent = input.value;
+        li.insertBefore(span,li.firstElementChild);
+        li.removeChild(input);
+        button.textContent = 'edit';
+      }
+    }
+    // select and run action in button's name
+    nameActions[action]();
+  }
+});
---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+var gulp      	= require('gulp'),
+		sass        = require('gulp-sass'),
+		browserSync = require('browser-sync'),
+		concat			= require('gulp-concat'),
+		minifyCSS   = require('gulp-minify-css'),
+		//concatCss		= require('gulp-concat-css'),
+		uglify			= require('gulp-uglifyjs'),
+		//cssnano			= require('gulp-cssnano'),
+		clean 			= require('gulp-clean'),
+		rename			= require('gulp-rename'),
+		del					= require('del'),
+		// imagemin		= require('gulp-imagemin'),
+		// pngquant		= require('imagemin-pngquant'),
+		// cache				= require('gulp-cache'),
+		autoprefixer= require('gulp-autoprefixer')
+		// sassdoc 		= require('sassdoc'),
+		// converter 	= require('sass-convert'),
+		//bourbon			= require('node-bourbon').includePaths,
+    //neat 				= require('node-neat').includePaths;
+
+var paths = {
+    scss: 'app/sass/**/*.scss'
+};
+
+// gulp.task('bourbon', function () {
+//     return gulp.src(paths.scss)
+//         .pipe(sass({
+// 						//includePaths: require('node-bourbon').includePaths,
+// 						includePaths: ['styles'].concat(neat),
+// 						//style: 'compressed',
+// 						//quiet: true
+//             //includePaths: ['styles'].concat(neat)
+//         }))
+//         .pipe(gulp.dest('app/css'));
+// });
+//
+// gulp.task('clean', ['sass'], function() {
+//   return gulp.src(['app/production/css', 'app/production/js',
+//   	'app/production/images'], {read: false})
+//     .pipe(clean());
+// });
+//
+// gulp.task('concat-styles', ['clean'], function() {
+//     return gulp.src([
+//     	'app/css/foundation.css',
+//     	'app/css/normalize.css',
+//     	'app/css/main.css'
+// 		])
+//     	.pipe(concat('main.min.css'))
+//         .pipe(minifyCSS({
+//             keepBreaks: true
+//         }))
+//         .pipe(gulp.dest('app/production/css'));
+// });
+//
+// gulp.task('sass', function(){
+// 	return	gulp.src('app/sass/main.scss')
+// 		.pipe(sass({
+// 			includePaths: require('node-bourbon').includePaths,
+// 			includePaths: require('node-neat').includePaths,
+// 			//includePaths: require('bitters').includePaths
+// 		}))
+// 		//.pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8', 'ie 7'], { cascade: true }))
+// 		.pipe(gulp.dest('app/css'))
+// 		.pipe(browserSync.reload({stream: true}));
+// });
+
+gulp.task('browser-sync', function() {
+
+	return browserSync({
+		server: {
+					baseDir: '.'
+				},
+		notify: false
+	});
+});
+
+gulp.task('sass:watch', ['browser-sync'], function(){
+
+	gulp.watch('*.html', browserSync.reload);
+	gulp.watch('**/*.js', browserSync.reload);
+});
+
+gulp.task('default', function(){
+	gulp.start('sass:watch');
+});
+
+// gulp.task('sass-convert', function() {
+//  return gulp.src('app/sass/**/theme.scss')
+//  	.pipe(converter({
+// 		from: 'scss',
+// 		to: 'sass',
+// 	}))
+// 		.pipe(rename('theme.sass'))
+// 		.pipe(gulp.dest('app/sass/sass'))
+// });
